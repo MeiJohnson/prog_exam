605fdab6799032342aecbba0417e8c403ba93687
MeiJohnson <52454580+MeiJohnson@users.noreply.github.com>
2021-06-24 00:15:19 +0300

    6 сем вср 11

--- ПРОГ-6/ВСР/vsr_11.py
+++ ПРОГ-6/ВСР/vsr_11.py
@@ -3,10 +3,20 @@ import numpy as np
 import time
 
 def main():
-    A = np.matrix('1 2 3; 4 5 6')
-    B = np.matrix('7 8; 9 1; 2 3')
-    C = A.dot(B)
-    print(C)
+    n_A = int(input('Введите размер n матрицы A '))
+    m_A = int(input('Введите размер m матрицы A '))
+    n_B = int(input('Введите размер n матрицы B '))
+    m_B = int(input('Введите размер m матрицы B '))
+    if(m_A == n_B):
+        A = np.array([[random.randint(1, 10) for j in range(m_A)] for i in range(n_A)])
+        print(A)
+        B = np.array([[random.randint(1, 10) for j in range(m_B)] for i in range(n_B)])
+        print(B)
+        C = A.dot(B)
+        print(C)
+    else:
+        print('Невозможно произвести умножение матриц заданного размера')
+    
 
 start_time = time.time()
 main()
